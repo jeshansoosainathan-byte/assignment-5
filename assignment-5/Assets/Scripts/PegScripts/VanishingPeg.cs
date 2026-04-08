0d9ff433fd2be4702c0d52b37377ec0bc3e020b9
Jsoosainathan <jsoosainathan@myseneca.ca>
2026-04-08 17:29:00 -0400

    add Reappear function

--- assignment-5/Assets/Scripts/PegScripts/VanishingPeg.cs
+++ assignment-5/Assets/Scripts/PegScripts/VanishingPeg.cs
@@ -1,12 +1,18 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class VanishingPeg : MonoBehaviour
 {
     private GameObject[] pegs;
+    List<int> disabled = new List<int>();
 
     void Start()
     {
         pegs = GameObject.FindGameObjectsWithTag("Peg");
+        InvokeRepeating(nameof(Vanish), 0f, 3f);
+         
     }
 
     // Update is called once per frame
@@ -14,4 +20,23 @@ public class VanishingPeg : MonoBehaviour
     {
         
     }
+
+    void Vanish()
+    {
+        int randomPeg = Random.Range(0, pegs.Length);
+
+        pegs[randomPeg].SetActive(false);
+
+        Invoke(nameof(ReAppear), 3f);
+
+    }
+
+    void ReAppear()
+    {
+        foreach (int i in disabled)
+        {
+            pegs[i].SetActive(true);
+        }
+    }
+
 }
